829fc113672fb552f1f07c69393e94fdb74f9f36
Rimaro03 <leonardoongaro4@gmail.com>
2023-12-27 09:21:14 +0100

    added iterator for players in class table

--- include/table.h
+++ include/table.h
@@ -4,14 +4,47 @@
 #include "human.h"
 #include "box.h"
 
-class Table {
+class Table
+{
 public:
-    Table(Player* players);
+    Table(Player *players);
     void turn();
+
+    struct Iterator
+    {
+        using iterator_category = std::random_access_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = Player;
+        using pointer = Player *;
+        using reference = Player &;
+
+        Iterator(pointer ptr) : player_ptr{ptr} {};
+        ~Iterator() {}
+        reference operator*() const { return *player_ptr; };
+        pointer operator->() { return player_ptr; };
+        Iterator &operator++()
+        {
+            player_ptr++;
+            return *this;
+        };
+        Iterator &operator++(int)
+        {
+            Iterator tmp = *this;
+            ++(*this);
+            return tmp;
+        };
+        friend bool operator==(const Iterator &a, const Iterator &b) { return a.player_ptr == b.player_ptr; };
+        friend bool operator!=(const Iterator &a, const Iterator &b) { return a.player_ptr != b.player_ptr; };
+    private:
+        pointer player_ptr;
+    };
+
+    Iterator begin() { return Iterator(&players_[0]); };
+    Iterator end() { return Iterator(&players_[4]); };
+
 private:
     Box map_[28];
-    Player* players_;
-
+    Player *players_;
 };
 
 #endif
--- src/table.cpp
+++ src/table.cpp
@@ -60,5 +60,4 @@ void Table::turn()
     {
         players_[i].turn();
     }
-    
 }
