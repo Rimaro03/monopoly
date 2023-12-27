9417dc7d017345fd0780f37f5e688f031a5e5390
Rimaro03 <leonardoongaro4@gmail.com>
2023-12-27 16:31:16 +0100

    bug fixed: table generation

--- src/table.cpp
+++ src/table.cpp
@@ -26,32 +26,40 @@ Table::Table()
 
     // randomizzazione indici con shuffle()
     std::shuffle(indici, indici + 24, std::default_random_engine(time(NULL)));
+    for (size_t i = 0; i < 24; i++)
+    {
+        std::cout << indici[i] << " ";
+    }
+
     int count = 0;
     for (unsigned int i = 0; i < 24; i++)
     {
-        Box* casella;
-        if (count < 8)
+        Box *casella;
+        if (i % 7 == 0)
         {
-            casella = new LateralBox(i, BoxType::economic, 6, 3, 3, 2, 4);
+            map_[i] = new SideBox(i, (i == 0));
         }
         else
         {
-            if (count < 10)
+            if (count < 8)
             {
-                casella = new LateralBox(i, BoxType::standard, 10, 5, 5, 4, 8);
+                casella = new LateralBox(i, BoxType::economic, 6, 3, 3, 2, 4);
             }
             else
             {
-                casella = new LateralBox(i, BoxType::luxury, 20, 10, 10, 7, 14);
+                if (count < 10)
+                {
+                    casella = new LateralBox(i, BoxType::standard, 10, 5, 5, 4, 8);
+                }
+                else
+                {
+                    casella = new LateralBox(i, BoxType::luxury, 20, 10, 10, 7, 14);
+                }
+                map_[indici[i]] = casella;
             }
-            map_[indici[i]] = casella;
+            count++;
         }
     }
-
-    map_[0] = new SideBox(0, true);
-    map_[7] = new SideBox(7, false);
-    map_[14] = new SideBox(14, false);
-    map_[21] = new SideBox(21, false);
 }
 
 Table::~Table()
@@ -60,10 +68,9 @@ Table::~Table()
     {
         delete map_[i];
     }
-    
 }
 
-void Table::players(Player* player1, Player* player2, Player* player3, Player* player4)
+void Table::players(Player *player1, Player *player2, Player *player3, Player *player4)
 {
     players_[0] = player1;
     players_[1] = player2;
@@ -75,12 +82,12 @@ void Table::turn()
 {
     for (size_t i = 0; i < 4; i++)
     {
-        //controllo partita finita o giocatore ha perso
+        // controllo partita finita o giocatore ha perso
         players_[i]->turn(this);
     }
 }
 
-Box** Table::box()
+Box **Table::box()
 {
     return map_;
 }
