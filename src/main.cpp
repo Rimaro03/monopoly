e79e5525d2fc47654fad54337f833139573ba01e
Davide <davide.tonno.dt@gmail.com>
2023-12-29 14:22:24 +0100

    added arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include "game.h"
 
-int main() {
-    int argc = 1;
-    const char* argv[] = { "human" }; // DEBUG
+int main(int argc, char* argv[]) {
+    if (argc != 2) { throw std::invalid_argument("args count must be 1!"); }
 
-    if (argc != 1) { throw std::invalid_argument("args count must be 1!"); }
-
-    Game::Init(argv[0]);
+    Game::Init(argv[1]);
     Game::Run();
     return 0;
 }
